50c19e8a0af9393d590f974d208b172de5d92c15
Jon Deaton <jonpauldeaton@gmail.com>
2018-08-16 17:04:27 -0700

    Started trying to add google performance benchmarking

--- bench/map-bench.c.h
+++ bench/map-bench.c.h
@@ -1,8 +1,14 @@
-//
-// Created by jdeaton on 8/16/18.
-//
-
 #ifndef LISP_MAP_BENCH_C_H
 #define LISP_MAP_BENCH_C_H
+
+#include <benchmark/benchmark.h>
+
+
+
+int main() {
+
+    return 0;
+}
+
 
 #endif //LISP_MAP_BENCH_C_H
